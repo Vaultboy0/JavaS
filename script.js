599a3ac1ccc32eb4221eb358e415490b4c632219
Vault_boy0 <oleg_95.95@mail.ru>
2023-02-09 17:43:30 +0300

    первое дз

---
+++ script.js
@@ -0,0 +1,13 @@
+// Задание 1
+let tempC = 30;
+let tempF = (9 / 5) * tempC + 32;
+console.log(`Температура в цельсиях ${tempC}, Температура в фаренгейтах ${tempF}`);
+
+
+
+
+
+//задание 2
+let name = "Oleg";
+let admin = name;
+console.log(admin);
